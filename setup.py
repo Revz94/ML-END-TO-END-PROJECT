17ce55db0b8d011640138745d825368406f47c9c
Revz94 <revathyremesh1994@gmail.com>
2023-03-16 10:03:17 +0530

    The second commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import find_packages,setup
+
+
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='REVATHY',
+author_email='revathyremesh1994@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+)
